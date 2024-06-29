c53dfee4cdca1aa35d65b6e3c7ee94a01bebedff
binsarjr <binsarjr121@gmail.com>
2024-06-29 22:32:53 +0700

    asdsad

---
+++ lua/plugins/laravel.lua
@@ -0,0 +1,15 @@
+return {
+  "adalessa/laravel.nvim",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "tpope/vim-dotenv",
+  },
+  cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
+  keys = {
+    { "<leader>la", ":Laravel artisan<cr>" },
+    { "<leader>lr", ":Laravel routes<cr>" },
+    { "<leader>lm", ":Laravel related<cr>" },
+  },
+  event = { "VeryLazy" },
+  config = true,
+}
