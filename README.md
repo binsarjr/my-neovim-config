da46ccf109af3a7d390d625ecb532515396fc2b6
binsarjr <binsarjr121@gmail.com>
2024-08-05 14:11:23 +0700

    docs: enhance README with personal editor setup and list supported languages; add supermaven plugin configuration

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # My Config Neovim
 
+For Now i use neovim as my main editor, and i use lazyvim to manage my plugins.
+
+## Language that i use
+
+- Typescript/Javascript
+- Lua
+
---
+++ lua/plugins/supermaven.lua
@@ -0,0 +1,23 @@
+local cmp = require("cmp")
+
+return {
+  {
+    "supermaven-inc/supermaven-nvim",
+    config = function()
+      require("supermaven-nvim").setup({
+        keymaps = {
+          accept_suggestion = "<C-]>",
+          clear_suggestion = "<C-\\>",
+          accept_word = "<C-j>",
+        },
+      })
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      table.insert(opts.sources, { name = "supermaven" })
+    end,
+  },
+}
