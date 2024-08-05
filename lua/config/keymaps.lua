f917767490370fde8616c7d3625ae3c247f07cc2
binsarjr <binsarjr121@gmail.com>
2024-08-05 13:28:11 +0700

    feat: add keymaps for NeoTree toggle and save all buffers in Neovim config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<S-e>", ":Neotree toggle<CR>", { desc = "NeoTree Reveal", silent = true })
+vim.keymap.set("n", "zz", ":wall<CR>", { desc = "Save All Buffer", silent = true })
