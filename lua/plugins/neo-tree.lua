4c1a60c7f13341ca111613ca69353b18931224cd
binsarjr <binsarjr121@gmail.com>
2024-08-05 14:13:56 +0700

    feat: enable optional image support and adjust NeoTree filesystem visibility settings in Neovim config

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,13 +6,22 @@ return {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+      "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
     opts = {
       filesystem = {
         filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
           hide_by_name = {
             "node_modules",
+            ".git",
+          },
+          never_show = {
+            "node_modules",
+            ".git",
           },
         },
       },
