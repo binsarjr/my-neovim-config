20050cbb0175a16897614ec7861dd7592d7cf1b2
binsarjr <binsarjr121@gmail.com>
2024-08-07 03:19:45 +0700

    feat: enable suggestion completion

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -10,38 +10,39 @@ cmp.setup({
     },
     documentation = {
       border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" }, -- Add border for the documentation menu
-      winblend = 0, -- Set the window blend to 0 to disable the transparency
+      winblend = 0,
     },
   },
-  -- formatting = {
-  --   format = function(entry, vim_item)
-  --     -- Customize the display of the completion window
-  --     vim_item.menu = ({
-  --       buffer = "[Buffer]",
-  --       nvim_lsp = "[LSP]",
-  --       luasnip = "[LuaSnip]",
-  --       nvim_lua = "[Lua]",
-  --       latex_symbols = "[Latex]",
-  --     })[entry.source.name]
-  --     return vim_item
-  --   end,
-  -- },
+  formatting = {
+    format = function(entry, vim_item)
+      -- Customize the display of the completion window
+      vim_item.menu = ({
+        buffer = "[Buffer]",
+        nvim_lsp = "[LSP]",
+        luasnip = "[LuaSnip]",
+        nvim_lua = "[Lua]",
+        latex_symbols = "[Latex]",
+        supermaven = "[SuperMaven]",
+      })[entry.source.name]
+      return vim_item
+    end,
+  },
   experimental = {
     ghost_text = false, -- Enable ghost text feature to avoid overlapping
   },
+  mapping = {
+    --   ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+    --   ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+    ["<S-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+    --   ["<C-e>"] = cmp.mapping({
+    --     i = cmp.mapping.abort(),
+    --     c = cmp.mapping.close(),
+    --   }),
+    --   ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Set `select` to `false` to disable automatic confirmation.
+    --   ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
+    --   ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
+  },
 
-  -- mapping = {
-  --   ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-  --   ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-  ["<S-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-  --   ["<C-e>"] = cmp.mapping({
-  --     i = cmp.mapping.abort(),
-  --     c = cmp.mapping.close(),
-  --   }),
-  --   ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Set `select` to `false` to disable automatic confirmation.
-  --   ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
-  --   ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
-  -- },
   -- other configurations...
 })
 
