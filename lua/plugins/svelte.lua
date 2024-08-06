80dd7cbe033c8b7f14340267fbf2f631119ea0be
binsarjr <binsarjr121@gmail.com>
2024-08-07 04:27:01 +0700

    feat: add svelte language server

---
+++ lua/plugins/svelte.lua
@@ -0,0 +1,59 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- add tsx and treesitter
+      vim.list_extend(opts.ensure_installed, {
+        "svelte",
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      opts.servers = opts.servers or {}
+      opts.servers.svelte = opts.servers.svelte or {}
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      -- set servers for svelte
+      opts.servers = opts.servers or {}
+
+      opts.servers.svelte = {
+        keys = {
+          {
+            "<leader>co",
+            LazyVim.lsp.action["source.organizeImports"],
+            desc = "Organize Imports",
+          },
+        },
+        capabilities = {
+          workspace = {
+            didChangeWatchedFiles = vim.fn.has("nvim-0.10") == 0 and { dynamicRegistration = true },
+          },
+        },
+      }
+
+      opts.servers.vtsls = opts.servers.vtsls or {}
+
+      LazyVim.extend(opts.servers.vtsls, "settings.vtsls.tsserver.globalPlugins", {
+        {
+          name = "typescript-svelte-plugin",
+          location = LazyVim.get_pkg_path("svelte-language-server", "/node_modules/typescript-svelte-plugin"),
+          enableForWorkspaceTypeScriptVersions = true,
+        },
+      })
+    end,
+  },
+  {
+    "conform.nvim",
+    opts = function(_, opts)
+      if LazyVim.has_extra("formatting.prettier") then
+        opts.formatters_by_ft = opts.formatters_by_ft or {}
+        opts.formatters_by_ft.svelte = { "prettier" }
+      end
+    end,
+  },
+}
