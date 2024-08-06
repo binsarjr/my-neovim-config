eb94b585dc38431780bd45f0876e8cb8e1e5a6c3
binsarjr <binsarjr121@gmail.com>
2024-08-07 03:07:26 +0700

    feat: separate mason on example.lua

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -4,6 +4,10 @@ return {
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
       table.insert(opts.ensure_installed, "typescript-language-server")
+      table.insert(opts.ensure_installed, "stylua")
+      table.insert(opts.ensure_installed, "shellcheck")
+      table.insert(opts.ensure_installed, "shfmt")
+      table.insert(opts.ensure_installed, "flake8")
     end,
   },
 }
