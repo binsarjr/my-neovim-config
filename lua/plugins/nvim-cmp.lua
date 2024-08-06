2f2a88268917640e0ecd1991282f82df1bc44bbf
binsarjr <binsarjr121@gmail.com>
2024-08-07 03:58:59 +0700

    feat: add prisma and sql

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -42,8 +42,32 @@ cmp.setup({
     --   ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
     --   ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
   },
+  sources = {
+    {
+      name = "dotenv",
+      -- Defaults
+      option = {
+        path = ".",
+        load_shell = true,
+        item_kind = cmp.lsp.CompletionItemKind.Variable,
+        eval_on_confirm = false,
+        show_documentation = true,
+        show_content_on_docs = true,
+        documentation_kind = "markdown",
+        dotenv_environment = ".*",
+        file_priority = function(a, b)
+          -- Prioritizing local files
+          return a:upper() < b:upper()
+        end,
+      },
+    },
+  },
 
   -- other configurations...
 })
 
-return {}
+return {
+  {
+    "SergioRibera/cmp-dotenv",
+  },
+}
