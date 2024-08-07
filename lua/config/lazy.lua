43a15d0259b02adcbd8dfb799332c3d6da401211
binsarjr <binsarjr121@gmail.com>
2024-08-07 07:05:10 +0700

    feat: enhance nvim setup with added support for JSON, Svelte, and Prettier; integrate supermaven plugin for improved completions

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,6 +20,9 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
     { import = "plugins" },
+    { import = "plugins.lang" },
+    { import = "plugins.formatter" },
+    { import = "plugins.copilot" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
