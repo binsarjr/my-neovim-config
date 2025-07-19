dca6a1d31a0b6d6675bc43ccf180f165750f3daa
binsarjr <binsarjr121@gmail.com>
2025-07-19 16:07:07 +0700

    feat: auto-update terminal title with current project name
    - Add autocmd to dynamically set terminal title based on current project directory
    - Update title on VimEnter, DirChanged, and BufEnter events
    - Display format: project-name - filename
    - Enable terminal title support with vim.opt.title = true
    
    Tested directory changes and buffer switching
    Terminal title updates correctly with project context

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,19 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Auto-update terminal title with current project name
+vim.api.nvim_create_autocmd({ "VimEnter", "DirChanged", "BufEnter" }, {
+  group = vim.api.nvim_create_augroup("TerminalTitle", { clear = true }),
+  callback = function()
+    local cwd = vim.fn.getcwd()
+    local project_name = vim.fn.fnamemodify(cwd, ":t")
+    if project_name and project_name ~= "" then
+      vim.opt.titlestring = project_name .. " - " .. vim.fn.expand("%:t")
+    else
+      vim.opt.titlestring = vim.fn.expand("%:t")
+    end
+  end,
+})
+
+vim.opt.title = true
